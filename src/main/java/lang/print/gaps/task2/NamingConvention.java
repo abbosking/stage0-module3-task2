26ab1dea04a4cd0ee0ccb2622b7aba1346137145
abbosking <105731575+abbosking@users.noreply.github.com>
2022-10-22 18:17:25 +0500

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,6 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variableOne = 1;
-        int variableTwo = 2;
-        int variableThree = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
+     //empty
     }
 }
